ad122e23dee1c21428b31d30a09789b60ed27039
sffxzzp <sffxzzp@users.noreply.github.com>
2019-08-19 19:57:21 +0800

    Update Clear_Linux.sh

--- caddy-rev/openssl/CA/Clear_Linux.sh
+++ caddy-rev/openssl/CA/Clear_Linux.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-
 rm -rf ./newcerts
 rm -rf ./Unblock
-rm -rf *.cer
-rm -rf *.pem
-rm -rf index.txt*
-rm -rf serial*
-echo '00' >serial
+rm -f *.cer
+rm -f *.pem
+rm -f index.txt*
+rm -f serial*
+touch index.txt
+echo '00'>serial
